f402babb600b89aa2eed4b087b062c8bc1b15b04
kaito-yamamoto <61260420+kaito-yamamoto@users.noreply.github.com>
2020-02-20 16:35:43 +0900

    Player
    おっきくなった

---
+++ GIL/Assets/PlayerScript/PlayerMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    int life;
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("ああああああ");
+        Debug.Log(life);
+    }
+}

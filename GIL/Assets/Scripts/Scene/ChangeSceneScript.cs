3ac8e100602297e99ec4bf9fc40947020f021f3c
yy-yusuke0122 <61263724+yy-yusuke0122@users.noreply.github.com>
2020-02-21 16:17:15 +0900

    結合用のタイトルとメインを一時的に作成

---
+++ GIL/Assets/Scripts/Scene/ChangeSceneScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneScript : MonoBehaviour
+{
+    public enum SceneType
+    {
+        Title = 0,
+        GameMain
+    }
+    [SerializeField]
+    SceneType changeType;
+
+    public void ChangeScene()
+    {
+        switch (changeType)
+        {
+            case SceneType.Title: SceneManager.LoadScene("Title"); break;
+            case SceneType.GameMain: SceneManager.LoadScene("GameMain"); break;
+        }
+    }
+}

a37126c654185e4b048b44863be2c2a2438f79a1
yy-yusuke0122 <61263724+yy-yusuke0122@users.noreply.github.com>
2020-02-23 11:56:19 +0900

    左右反転・壁の張り付きを解決しました

---
+++ GIL/Assets/Scripts/Player/L_PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L_PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 右移動時
+        transform.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 10);
+        // 左移動時
+        transform.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 10);
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
---
+++ GIL/Assets/Scripts/Player/PlayerDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerDataProvider : MonoBehaviour
+{
+    [SerializeField] private PlayerController playerController = null;
+
+    public PlayerStateEnum IsPlayerStateEnum { get { return playerController.playerState; } }//自機の状態
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

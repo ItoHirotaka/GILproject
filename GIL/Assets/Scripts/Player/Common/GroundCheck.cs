e112995ebd39d0d6402abac41b961b170c5e0d48
yy-yusuke0122 <61263724+yy-yusuke0122@users.noreply.github.com>
2020-02-25 12:16:56 +0900

    現在のベースを作りました

---
+++ GIL/Assets/Scripts/Player/Common/GroundCheck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    const int ICE_LAYER = 12;
+    GameObject gameSystem = null;
+    GameObject player = null;
+    Rigidbody2D rb = null;
+    private void Start()
+    {
+        gameSystem = GameObject.Find("GameSystem");
+        player = GameObject.Find("Player");
+        rb = player.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        bool isGround = collision.gameObject.tag == "Stage";
+        if (isGround && isDeath())
+        {
+            gameSystem.GetComponent<GameSystem2D>().Death();
+        }
+    }
+
+    [SerializeField]
+    float DeathFall = -13f;  // 死亡判定にする速さ
+    bool isDeath()
+    {
+        bool isDeathHight = rb.velocity.y < DeathFall;
+        bool isIce = player.layer == ICE_LAYER;
+        return isDeathHight && isIce;
+    }
+}

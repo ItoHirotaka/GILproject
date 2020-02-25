90a4006740d9c91a6e2ef65e326245b40e370608
Hirotaka_I <Hirotaka_ito_1021@yahoo.co.jp>
2020-02-25 15:00:11 +0900

    音、死画像

--- GIL/Assets/Scripts/Stage/System/2D/DeathDirector2D.cs
+++ GIL/Assets/Scripts/Stage/System/2D/DeathDirector2D.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DeathDirector2D : MonoBehaviour
 {
+
+    //死亡音
+    public AudioClip DeathSound;
+    AudioSource audioSource;
+
     [SerializeField]
     GameObject FadeImage = null;
     FadeUi fadeUi;
 
     [SerializeField]
     GameObject player = null;
+
+    //イメージを取得
+    [SerializeField]
+    GameObject image_object;
+    [SerializeField]
+    Image image;
+
+    //画像のカラー取得
+    float red, green, blue, alpha;
 
     bool isDeath;
     void Start()
@@ -19,7 +34,16 @@ public class DeathDirector2D : MonoBehaviour
         fadeUi = FadeImage.GetComponent<FadeUi>();
         // 死亡状態の初期化
         isDeath = false;
+        //音のコンポーネントを取得
+        audioSource = GetComponent<AudioSource>();
+        // オブジェクトの取得
+        image_object = GameObject.Find("Image");
+        red = image.color.r;
+        green = image.color.g;
+        blue = image.color.b;
+        alpha = image.color.a;
     }
+
 
     void Update()
     {
@@ -32,6 +56,8 @@ public class DeathDirector2D : MonoBehaviour
     [SerializeField]
     float DontControlTime = 4f; // テストの値です。Inspectorで調整してください
     float count;
+    //アルファ値の最大値
+    float MaxAlpha = 255f;
     // 死亡状態をセット
     public void Death()
     {
@@ -43,6 +69,10 @@ public class DeathDirector2D : MonoBehaviour
         fadeUi.ChangeState(FadeUi.FadeState.FadeIn);
         // プレイヤーの操作を無効化
         player.GetComponent<PlayerController2D>().UseControl(false);
+        //音出力
+        audioSource.PlayOneShot(DeathSound);
+        alpha = MaxAlpha;
+        SetAlpha();
     }
 
     // 死亡時の更新処理
@@ -54,5 +84,9 @@ public class DeathDirector2D : MonoBehaviour
         {
             SceneManager.LoadScene("GameMain2D");
         }
+    }
+    void SetAlpha()//アルファ値の変更
+    {
+        image.color = new Color(red, green, blue, alpha);
     }
 }

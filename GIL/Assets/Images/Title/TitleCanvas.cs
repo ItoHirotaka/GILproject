47ce678e86518205000d4942a6ca35df1ce7cf40
kaito-yamamoto <61260420+kaito-yamamoto@users.noreply.github.com>
2020-02-26 15:27:35 +0900

    タイトル

---
+++ GIL/Assets/Images/Title/TitleCanvas.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleCanvas : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] TitleImage;
+
+    int currentNum;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("StageNum"))
+        {
+            currentNum = PlayerPrefs.GetInt("StageNum");
+        }
+        else
+        {
+            currentNum = 0;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentNum -= 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentNum += 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            PlayerPrefs.SetInt("StageNum", currentNum);
+            SceneManager.LoadScene("GameMain2D");
+        }
+    }
+
+    private void Change()
+    {
+        
+    }
+}

e5b2c1316ecac6d346c9030b0bb9f26bdf2b7540
yy-yusuke0122 <61263724+yy-yusuke0122@users.noreply.github.com>
2020-02-27 12:22:00 +0900

    test

---
+++ GIL/Assets/TransformSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformSound : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip audio;
+
+    public void PlaySound()
+    {
+        AudioSource.PlayClipAtPoint(audio, this.transform.position);
+    }
+}

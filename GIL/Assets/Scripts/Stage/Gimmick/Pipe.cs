7f6cb2cec7b8a8250b24b14507ba41d86ab47a08
satou-yuusike <61260423+satou-yuusike@users.noreply.github.com>
2020-02-27 13:54:04 +0900

    ステージ

---
+++ GIL/Assets/Scripts/Stage/Gimmick/Pipe.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    [SerializeField]
+    GameObject outPos;
+    Transform tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = outPos.transform;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 10)
+        {
+            collision.transform.position = tr.position;
+        }
+    }
+}
